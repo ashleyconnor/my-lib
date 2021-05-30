7bfa0c59d77c3ffb0194d24933cb93c265ba8fd7
Ashley Connor <me@ashleyconnor.co.uk>
2021-05-30 15:30:30 -0400

    API extractor basic setup

---
+++ src/subtract.ts
@@ -0,0 +1,13 @@
+
+/**
+ * Calculate the difference between two numbers
+ * 
+ * @param a - first number
+ * @param b - second number
+ * @returns the difference between the first and second number  
+ * 
+ * @beta
+ */
+export function subtract(a: number, b = 0): number {
+    return a - b;
+}
